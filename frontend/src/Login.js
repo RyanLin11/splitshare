c911167cf0345ff43fd19610391fccf6e00836ce
Edward Wang <edwardw483@gmail.com>
2023-10-14 17:21:59 -0400

    navigate pages

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,5 +1,12 @@
-function Login() {
+import { useEffect, useState } from 'react';
 
+function Login() {
+    const [email, setEmail] = useState(email);
+    return (
+        <div>
+            <input name='email' type='email' />
+        </div>
+    )
 }
 
 export default Login;
